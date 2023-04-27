7f8e8cc21c6780a8f5e7dd14344600456014e5d8
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-27 19:05:59 +0000

    Add typed text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Typed from 'react-typed';
 
 const Header = () => {
   return (
     <div className="header-wraper">
       <div className="main-info">
         <h1>web development and websites promotions</h1>
+        <Typed 
+          className="typed-text"
+          string={["web Design", "web Developement", "Facebook Ads SMM", "Google Ads"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
       </div>
     </div>
   )
