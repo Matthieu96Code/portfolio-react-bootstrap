0ceb5232bc50dac560ec6e9626222210e50db3e2
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-27 16:20:16 +0000

    Add hamburger icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import logo from '../logo.png'
+// react font awesome import
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 
 const Navbar = () => {
   return (
@@ -7,12 +11,13 @@ const Navbar = () => {
   <div className="container">
     <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="matt log" /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      {/* <span className="navbar-toggler-icon"></span> */}
+      <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">about me</a>
