ca9e8507fa7e24490a0f5df23db65d3dc68ae2c9
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-29 11:16:35 +0000

    Add and comment particles problematic code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+// import { tsParticles } from "tsparticles-engine";
+// import Particles from 'react-particles';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 
 function App() {
   return (
     <>
+      {/* <Particles 
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            }
+          }
+        }}
+      /> */}
       <Navbar />
       <Header />
     </>
