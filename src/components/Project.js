d4395a92d6f1ab6113bd37b470bf97f73c17f41d
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-11 15:53:59 +0000

    Create card for every project

---
+++ src/components/Project.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <section className="card">
+      <h2 className="card-title">project 1</h2>
+      <p className="card-text">A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.</p>
+      <img src="" alt="project-snipping"/>
+      <ul className="technologies"></ul>
+      <div className="card-link">
+        <button className="btn btn-outline-warning">css</button>
+        <button className="btn btn-outline-warning ">html</button>
+        <button className="btn btn-outline-warning">Java Script</button>
+      </div>
+      
+    </section>
+  )
+}
+
+export default Project
