d4e1ddcb9fa1ac142954b7894e9d7d437ee66c25
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-25 22:01:27 +0000

    Add dummy data on props

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Project = () => {
+const Project = (props) => {
+
+  const project = props.project
   return (
     <section className="card">
-      <h2 className="card-title">project 1</h2>
-      <img src="" alt="project-snipping"/>
-      <p className="card-text">A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.</p>
+      <h2 className="card-title">{project.title}</h2>
+      <img src={project.img} alt={project.alt}/>
+      <p className="card-text">{project.description}</p>
       <div className="card-link">
         <button className="btn btn-outline-danger">css</button>
         <button className="btn btn-outline-danger">html</button>
@@ -17,3 +19,4 @@ const Project = () => {
 }
 
 export default Project
+
--- src/components/works.js
+++ src/components/works.js
@@ -2,32 +2,43 @@ import React from 'react'
 import Project from './Project'
 
 const Works = () => {
+
+  const projectList = [
+    {
+      title: 'project 1',
+      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+      img: 'https://picsum.photos/id/20/200/300',
+      alt: 'project one  picture',
+      link: ''
+    }
+  ]
+
   return (
     <div className="bg-light">
-      <div className="container">
+      <div className="px-5">
         <h2 className="display-3 text-center pt-5">Works</h2>
 
         <div className="row py-5">
           <div className="col">
-            <Project />
+            <Project project = {projectList[0]} />
           </div>
           <div className="col">
-            <Project />
+            <Project project = {projectList[0]} />
           </div>
           <div className="col">
-            <Project />
+            <Project project = {projectList[0]} />
           </div>
         </div>
 
         <div className="row py-5">
           <div className="col">
-            <Project />
+            <Project project = {projectList[0]} />
           </div>
           <div className="col">
-            <Project />
+            <Project project = {projectList[0]} />
           </div>
           <div className="col ">
-            <Project />
+            <Project project = {projectList[0]} />
           </div>
         </div>
 
