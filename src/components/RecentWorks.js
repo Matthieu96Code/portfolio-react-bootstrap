bdca74f190105df71c75f3433d698c5507867ba6
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-08-09 12:23:01 +0000

    Create recent work and skills data files

---
+++ src/components/RecentWorks.js
@@ -0,0 +1,121 @@
+const projectList = [
+    {
+        id: 1,
+        title: 'Togoville Jazz 2022',
+        description: 'Togoville Jazz 2022 is a jazz festival website featuring two pages, multiple sections, and captivating HTML, CSS, and JavaScript animations. With a visually stunning design, it offers event details, artist profiles, and engaging interactivity. This immersive platform connects jazz enthusiasts, artists, and organizers for an unforgettable online celebration of music and culture.',
+        img: './img/togoville jazz project.PNG',
+        alt: 'Togoville Jazz 2022 project picture',
+        link: 'https://matthieu96code.github.io/capstone-project1-concert/',
+        source: 'https://github.com/Matthieu96Code/capstone-project1-concert',
+        technologies: [
+            {id: 1, name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML'},
+            {id: 2, name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS'},
+            {id: 3, name: 'JavaScript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
+        ]
+    },
+    {
+        id: 2,
+        title: 'Math Magicians',
+        description: 'Math Magicians is a captivating Single Page App (SPA) designed for mathematics enthusiasts. It features a user-friendly interface for simple calculations and provides random math-related quotes, sparking inspiration. With its minimalist design and interactive features, it offers an engaging platform for all fans of mathematics.',
+        img: './img/Math magicians.PNG',
+        alt: 'Math Magicians project picture',
+        link: 'https://math-magicians-6gfp.onrender.com/',
+        source: 'https://github.com/Matthieu96Code/math-magicians',
+        technologies: [
+            {id: 1, name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML'},
+            {id: 2, name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS'},
+            {id: 3, name: 'JavaScript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
+            {id: 4, name: 'React', link: 'https://reactjs.org/'},
+            {id: 5, name: 'Redux', link: 'https://redux.js.org/'},
+            {id: 6, name: 'Rest API', link: 'https://developer.mozilla.org/en-US/docs/Glossary/REST'},
+        ]
+    },
+    {
+        id: 3,
+        title: 'Leader board',
+        description: 'The Leaderboard website is a platform that showcases scores submitted by various players. With a sleek and user-friendly interface, it allows users to view and compare scores from different players across various games or activities. The website dynamically updates in real-time as new scores are submitted, providing a dynamic and engaging experience for participants and spectators alike. Stay informed, track progress, and foster healthy competition through the Leaderboard website.',
+        img: './img/Leader board.PNG',
+        alt: 'Leader board project picture',
+        link: 'https://matthieu96code.github.io/leaderboard-project/dist/',
+        source: 'https://github.com/Matthieu96Code/leaderboard-project',
+        technologies: [
+            {id: 1, name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML'},
+            {id: 2, name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS'},
+            {id: 3, name: 'JavaScript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
+            {id: 4, name: 'React', link: 'https://reactjs.org/'},
+            {id: 5, name: 'Redux', link: 'https://redux.js.org/'},
+            {id: 6, name: 'Rest API', link: 'https://developer.mozilla.org/en-US/docs/Glossary/REST'},
+        ]
+    },
+    {
+        id: 4,
+        title: 'Currency metrics webapp',
+        description: 'The Currency Metrics web app is a responsive application built using React and Redux. The app consists of two pages. The first page displays a comprehensive list of currency codes and names, providing users with an overview of various currencies. On the second page, users can select a specific currency and view its corresponding stock information.',
+        img: './img/currencies metrics.PNG',
+        alt: 'Currency metrics webapp project picture',
+        link: 'https://metrics-webapp-jwa8.onrender.com/',
+        source: 'https://github.com/Matthieu96Code/metrics-webapp',
+        technologies: [
+            {id: 1, name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML'},
+            {id: 2, name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS'},
+            {id: 3, name: 'JavaScript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
+            {id: 4, name: 'React', link: 'https://reactjs.org/'},
+            {id: 5, name: 'Redux', link: 'https://redux.js.org/'},
+            {id: 6, name: 'Rest API', link: 'https://developer.mozilla.org/en-US/docs/Glossary/REST'},
+        ]
+    },
+    {
+        id: 5,
+        title: 'Bookstore',
+        description: 'Bookstore is a straightforward website that showcases a book list and offers functionality to add and remove books. With a user-friendly interface, users can easily navigate through the displayed books, seamlessly add new entries, and remove existing ones, making it a convenient platform for managing book collections.',
+        img: './img/bookstore.PNG',
+        alt: 'Bookstore project picture',
+        link: '',
+        source: 'https://github.com/Matthieu96Code/bookstore',
+        technologies: [
+            {id: 1, name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML'},
+            {id: 2, name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS'},
+            {id: 3, name: 'JavaScript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
+            {id: 4, name: 'React', link: 'https://reactjs.org/'},
+            {id: 5, name: 'Redux', link: 'https://redux.js.org/'},
+            {id: 6, name: 'Rest API', link: 'https://developer.mozilla.org/en-US/docs/Glossary/REST'},
+        ]
+    },
+    {
+        id: 6,
+        title: 'Budget App',
+        description: 'budget-app is a mobile web application built using Ruby on Rails that helps users manage their budget effectively. With this app, users can register an account and log in, ensuring that their data remains private and secure.',
+        img: './img/Budget app.PNG',
+        alt: 'Budget App project picture',
+        link: 'https://budget-7a3k.onrender.com/',
+        source: 'https://github.com/Matthieu96Code/budget-app',
+        technologies: [
+            {id: 1, name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML'},
+            {id: 2, name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS'},
+            {id: 3, name: 'Ruby', link: 'https://www.ruby-lang.org/en/'},
+            {id: 4, name: 'Ruby on Rails', link: 'https://rubyonrails.org/'},
+        ]
+    },
+]
+
+export const projectUnused = [
+    {
+        id: 7,
+        title: 'Space Traveler\'s Hub',
+        description: 'Space Traveler\'s Hub is a dynamic website built with React and Redux. It offers an immersive platform for users to explore space missions and rockets. Users can join missions, reserve rockets, and access detailed information about each mission and rocket, creating an engaging experience for space enthusiasts and aspiring astronauts.',
+        img: './img/rockets.png',
+        alt: 'Space Traveler\'s Hub project picture',
+        link: '',
+        source: 'https://github.com/Matthieu96Code/spaceTravelersHub',
+        technologies: [
+            {id: 1, name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML'},
+            {id: 2, name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS'},
+            {id: 3, name: 'JavaScript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
+            {id: 4, name: 'React', link: 'https://reactjs.org/'},
+            {id: 5, name: 'Redux', link: 'https://redux.js.org/'},
+            {id: 6, name: 'Rest API', link: 'https://developer.mozilla.org/en-US/docs/Glossary/REST'},
+        ]
+    },
+];
+
+export default projectList;
---
+++ src/components/Skills.js
@@ -0,0 +1,44 @@
+const skills = [
+    {
+        id: 1,
+        category: 'Frontend',
+        skills: [
+            { id: 1, name: 'HTML5', link: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5' },
+            { id: 2, name: 'CSS3', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+            { id: 3, name: 'JavaScript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+            { id: 4, name: 'React', link: 'https://reactjs.org/' },
+            { id: 5, name: 'Redux', link: 'https://redux.js.org/' },
+            { id: 6, name: 'Bootstrap', link: 'https://getbootstrap.com/' },
+            { id: 7, name: 'jQuery', link: 'https://jquery.com/' },
+        ]
+    },
+    {
+        id: 2,
+        category: 'Backend',
+        skills: [
+            { id: 1, name: 'Ruby', link: 'https://www.ruby-lang.org/en/' },
+            { id: 2, name: 'Ruby on Rails', link: 'https://rubyonrails.org/' },
+            { id: 3, name: 'MySQL', link: 'https://www.mysql.com/' },
+            { id: 4, name: 'PostgreSQL', link: 'https://www.postgresql.org/' },
+            { id: 5, name: 'RESTful API', link: 'https://restfulapi.net/' },
+            { id: 6, name: 'JSON', link: 'https://www.json.org/json-en.html' },
+        ]
+    },
+    {
+        id: 3,
+        category: 'Tools',
+        skills: [
+            { id: 1, name: 'Git', link: 'https://git-scm.com/' },
+            { id: 2, name: 'GitHub', link: 'https://docs.github.com/' },
+            { id: 3, name: 'Visual Studio Code', link: 'https://code.visualstudio.com/' },
+            { id: 4, name: 'Rspec', link: 'https://rspec.info/' },
+            { id: 5, name: 'jest', link: 'https://jestjs.io/' },
+            { id: 6, name: 'chrome devtools', link: 'https://developer.chrome.com/docs/devtools/' },
+            { id: 7, name: 'Heroku', link: 'https://www.heroku.com/' },
+            { id: 8, name: 'Netlify', link: 'https://www.netlify.com/' },
+            { id: 9, name: 'Responsive Design', link: 'https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Responsive_Design' },
+        ]
+    }     
+]
+
+export default skills
