d6fc7b7375ac4e9737ddc316120e129f4f492404
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-08-09 12:33:28 +0000

    Add css styling

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,61 +1,28 @@
 import React from 'react'
+import skills from './Skills'
 
 const Technologies = () => {
   return (
     <div>
-      <div class="accordion" id="accordionPanelsStayOpenExample">
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-              Languages
+      <div className="accordion" id="accordionPanelsStayOpenExample">
+        {skills.map((skill) => (
+        <div key={skill.id} className="accordion-item">
+          <h2 className="accordion-header" id={`panelsStayOpen-heading${skill.category}`}>
+            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#panelsStayOpen-collapse${skill.category}`} aria-expanded="true" aria-controls={`panelsStayOpen-collapse${skill.category}`}>
+              {skill.category}
             </button>
           </h2>
-          <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-            <div class="accordion-body">
-              <ul class="list-group list-group-horizontal">
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">HTML</a></li>
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">CSS</a></li>
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">JavaScript</a></li>
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">Ruby</a></li>
+          <div id={`panelsStayOpen-collapse${skill.category}`} class="accordion-collapse collapse show" aria-labelledby={`panelsStayOpen-heading${skill.category}`}>
+            <div className="accordion-body">
+              <ul className="list-group list-group-horizontal row row-cols-auto">
+                {skill.skills.map((item) => (
+                  <li className="list-group-item col"><a href={item.link} className="btn btn-outline-success">{item.name}</a></li>
+                ))}
               </ul>
             </div>
           </div>
         </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-              Frameworks
-            </button>
-          </h2>
-          <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-            <div class="accordion-body">
-              <ul class="list-group list-group-horizontal">
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">React.js</a></li>
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">Ruby on Rails</a></li>
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">Rspec</a></li>
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">Redux</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-              Skills
-            </button>
-          </h2>
-          <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-            <div class="accordion-body">
-              <ul class="list-group list-group-horizontal">
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">Database Management</a></li>
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">Version Control</a></li>
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">CLI</a></li>
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">API Design</a></li>
-                <li class="list-group-item"><a href="#" className="btn btn-outline-danger">Web Development</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   )
