3880d56f9d5e9c4b518853ed4d4f732b3cd6bca3
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-08-09 15:15:29 +0000

    Add responsivness on contact form

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
       <Header />
       <Works />
       <AboutMe />
-      <hr />
       <Contacts />
     </>
   );
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,31 +2,30 @@ import React from 'react'
 
 const Contacts = () => {
   return (
-    <div className="bg-dark py-5" id="contacts">
-      <p className="container-sm text-light text-center display-6 col-5 pb-5">I'm always interested in hearing about new projects, so if you'd like to chat please get in touch.</p>
-      <div className="container-sm col-5">
-        <div className="col-5"></div>
+    <div className="bg-light py-5 row row-cols-1 row-cols-xl-2" id="contacts">
+      <p className="container-sm text-dark text-center display-6 col p-5">I'm always interested in hearing about new projects, so if you'd like to chat please get in touch.</p>
+      <div className="container-sm col px-5">
         <form className="container-sm col text-center" action="https://formspree.io/f/mlekdbwd" method="post">
           <div class="mb-3 mt-3">
-            <label for="fullname" class="form-label">Full name:</label>
+            <label for="fullname" class="form-label" />
             <input type="text" class="form-control" id="fullname" placeholder="Full name" name="fullname"/>
           </div>
           <div class="mb-3 mt-3">
-            <label for="email" class="form-label">Email:</label>
+            <label for="email" class="form-label" />
             <input type="email" class="form-control" id="email" placeholder="Email address" name="email"/>
           </div>
           <div class="mb-3">
-            <label for="comment">Comments:</label>
+            <label for="comment" />
             <textarea class="form-control" rows="5" id="comment" name="text"></textarea>
           </div>
-          <button type="submit" class="col-4 btn btn-primary my-3">Submit</button>
+          <button type="submit" class="col-md-3 col-lg-4 btn btn-primary my-3">Submit</button>
         </form>
-      </div>
-      <div className='d-flex justify-content-center my-5'>
+        <div className='d-flex justify-content-center mt-5'>
         <a className='mx-3 h4' href="https://www.linkedin.com/in/kodjo-matthieu-senou/" target="blank"><i className="bi bi-linkedin"></i></a>
         <a className='mx-3 h4' href="https://github.com/Matthieu96Code" target="blank"><i className="bi bi-github"></i></a>
         <a className='mx-3 h4' href="https://twitter.com/MatthieuSenou" target="blank"><i className="bi bi-twitter"></i></a>
         <a className='mx-3 h4' href="https://www.facebook.com/matthieu.senou/" target="blank"><i className="bi bi-facebook"></i></a>
+      </div>
       </div>
     </div>
   )
