d19e2b64cbedb102bb19c784545dc2818316a6a4
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-08-15 22:26:22 +0000

    Update intro and about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,7 +9,8 @@ const AboutMe = () => {
           <div className="col px-5">
             <h2 className="display-3 text-light text-danger">About me</h2>
 
-            <p className="text-light h4 py-4">Diving deep into the realm of remote development, I've dedicated substantial hours to creating a diverse range of projects, spanning from dynamic landing pages to seamless API integrations. My academic journey reflects a remarkable achievement. This attests to my capacity for strategic and effective work, even when facing formidable challenges.</p>
+            <p className="text-light h4 py-4">Embarking on an exhilarating journey into the realm of remote development, I've poured countless hours into crafting a diverse array of projects. From designing dynamic landing pages that invite exploration to seamlessly integrating APIs, my portfolio is a testament to unbounded creativity and unwavering dedication.</p>
+            <p className="text-light h4 py-4">My academic path stands as a monument to remarkable achievements, highlighting my skill in strategic thinking and unwavering determination when faced with difficult challenges. As you delve into my world of accomplishments, get ready to be inspired by the artistry of innovation and the strength of achievement.</p>
           </div>
           <div className="col p-5">
             <Technologies />
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,19 @@ const Header = () => {
     <div className="header-wraper" id="header">
       <div className="main-info">
         <h1 className='text-center'>Hey there. I’m Matthieu Senou</h1>
-        <h2 className='text-center'>A Full-Stack Web developer</h2>
-        <p className='my-5 col-9 h3 text-light text-center'>I'm excited to build an impressive web app with strong skills in JavaScript, React, Redux, Ruby, and Ruby on Rails. I'm also aware of new technologies and passionate about gaming and artificial intelligence.</p>
+        {/* <h2 className='text-center'>A Full-Stack Web developer</h2> */}
         <Typed 
           className="typed-text"
-          strings={["Web Design", "Web Developement", "Testing", "Maintenance"]}
+          strings={["A Full-Stack Web developer", "A Software Engineer", "A Tech Enthusiast"]}
           typeSpeed={40}
           backSpeed={60}
           loop
         />
-        <a href="https://www.senoumatthieu96@gmail.com" target="blank" className="btn-main-offer">contact me</a>
+        <p className='my-5 col-9 h4 text-light text-center'>Prepare to be captivated as I embark on crafting an extraordinary web app, seamlessly blending my expertise in JavaScript, React, Redux, Ruby, and Ruby on Rails. But that's just the tip of the iceberg – I'm an avid explorer of emerging technologies, fueled by a fervent passion for gaming and artificial intelligence. Brace yourself for an innovative journey that promises to redefine your digital experience, one line of code at a time.</p>
+        <div className='mb-5'>
+          <a href="https://www.senoumatthieu96@gmail.com" target="blank" className="btn-main-offer">mailed me</a>
+          <a href="#contacts" className="btn-main-offer text-fw-4">contact me</a>
+        </div>
         <div className='row'>
           <a className='col h4' href="https://www.linkedin.com/in/kodjo-matthieu-senou/" target="blank"><i className="bi bi-linkedin"></i></a>
           <a className='col h4' href="https://github.com/Matthieu96Code" target="blank"><i className="bi bi-github"></i></a>
