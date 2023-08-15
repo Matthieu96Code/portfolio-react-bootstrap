61cd5ddb38d4c1a8784be376fd90e021074729b1
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-08-15 11:01:06 +0000

    Stick the nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,20 @@
 import React from 'react'
-import logo from '../logo.png'
-// react font awesome import
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-dark">
       <div className="container">
-        <a className="navbar-brand pt-3 text-light" href="#">Matthieu</a>
+        <a className="navbar-brand pt-3 text-light" href="#header">Matthieu</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 my-lg-0">
             <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">Home</a>
+              <a className="nav-link" aria-current="page" href="#header">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#works">works</a>
